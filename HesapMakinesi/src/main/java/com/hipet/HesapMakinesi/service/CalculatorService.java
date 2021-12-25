c879d1ba4d6dc3e45c1366751eea27904a119649
SalihSulak <salihsulak00@gmail.com>
2021-12-25 13:21:35 +0300

    Add 4 operation

--- HesapMakinesi/src/main/java/com/hipet/HesapMakinesi/service/CalculatorService.java
+++ HesapMakinesi/src/main/java/com/hipet/HesapMakinesi/service/CalculatorService.java
@@ -4,14 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalculatorService {
-	public double sum(ArrayList<Double> numbers) {
-		double sum=0;
-		 for(double number:numbers)  {
-			 sum += number;  
-			}  
+	public double sum(double number1, double number2) {
+		
+			double sum=0;
+			sum = number1-number2;
 		return sum;
 	}
 	
-
+	public double extraction(double number1, double number2) {
+		
+			double sum=0;
+			sum = number1-number2;
+		return sum;
+	}
 	
+	public double multiplication(double number1, double number2) {
+		
+			double sum=1;
+			sum = number1*number2;
+		return sum;
+	}
+	
+	public double division(double number1, double number2) {
+		
+			double sum=1;
+			sum = number1*number2;
+		return sum;
+	}
 }
