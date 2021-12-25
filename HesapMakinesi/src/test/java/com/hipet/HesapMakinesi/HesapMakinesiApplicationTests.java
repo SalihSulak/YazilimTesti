4b229b01e0a490828cd4281eeb7933b49386a148
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-12-25 12:35:18 +0300

    Initial Commit

---
+++ HesapMakinesi/src/test/java/com/hipet/HesapMakinesi/HesapMakinesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hipet.HesapMakinesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HesapMakinesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
