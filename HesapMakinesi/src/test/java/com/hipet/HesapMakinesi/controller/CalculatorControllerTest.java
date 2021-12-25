790328f039edfab32033eb3f153fc301aee98ac9
SalihSulak <salihsulak00@gmail.com>
2021-12-25 17:42:57 +0300

    Update CalculatorControllerTest
    Co-Authored-By: Tuğba Nur Bayram <57587607+tugbanurbayram@users.noreply.github.com>
    Co-Authored-By: Mehmet Berkan <80527393+MBerkanGr@users.noreply.github.com>
    Co-Authored-By: Şebnem <72860809+SebnemGunes@users.noreply.github.com>

--- HesapMakinesi/src/test/java/com/hipet/HesapMakinesi/controller/CalculatorControllerTest.java
+++ HesapMakinesi/src/test/java/com/hipet/HesapMakinesi/controller/CalculatorControllerTest.java
@@ -51,4 +51,49 @@ class CalculatorControllerTest {
         double result = calculatorController.multiplication(number1,number2);
         assertEquals(36,result);
     }
+    @Test
+    void division() {
+    	 double number1 = 6;
+         double number2 = 6;
+         double realResult = number1/number2;
+         when(calculatorService.division(number1,number2)).thenReturn(realResult);
+         double result = calculatorController.division(number1,number2);
+         assertEquals(1,result);
+    	
+    }
+    @Test
+   	void log() {
+       	     double number1 = 10;
+             double realResult = Math.log10(10);
+             when(calculatorService.log(number1)).thenReturn(realResult);
+             double result = calculatorController.log(number1);
+             assertEquals(1,result);
+       	
+    	
+    }
+    @Test
+    void square() {
+    	double number1 = 6;
+        double realResult = number1*number1;
+        when(calculatorService.square(number1)).thenReturn(realResult);
+        double result = calculatorController.square(number1);
+        assertEquals(36,result);
+    }
+    @Test
+    void cub() {
+    	double number1= 3;
+    	double realResult = number1*number1*number1;
+    	when(calculatorService.cub(number1)).thenReturn(realResult);
+    	double result = calculatorController.cub(number1);
+    	assertEquals(27,result);
+    	
+    }
+    @Test    
+    void squareRoot() {
+    	double number1= 25;
+    	double realResult = Math.sqrt(number1);
+    	when(calculatorService.squareRoot(number1)).thenReturn(realResult);
+    	double result = calculatorController.squareRoot(number1);
+    	assertEquals(5,result);
+    }     
 }
